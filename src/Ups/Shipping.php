847a893aa61877a2156d78017736bd3a883d370c
martijn-dd <martijn@dutchdrops.com>
2016-02-03 13:09:51 +0100

    Reference weer uit

--- src/Ups/Shipping.php
+++ src/Ups/Shipping.php
@@ -596,16 +596,16 @@ class Shipping extends Ups
                 $node->appendChild($xml->createElement('LargePackageIndicator'));
             }
 
-            if (isset($package->ReferenceNumber)) {
-                $refNode = $node->appendChild($xml->createElement('ReferenceNumber'));
+            //if (isset($package->ReferenceNumber)) {
+            //    $refNode = $node->appendChild($xml->createElement('ReferenceNumber'));
 
               //  if ($package->ReferenceNumber->BarCodeIndicator) {
               //      $refNode->appendChild($xml->createElement('BarCodeIndicator', $package->ReferenceNumber->BarCodeIndicator));
               //   }
 
-                $refNode->appendChild($xml->createElement('Code', $package->ReferenceNumber->Code));
-                $refNode->appendChild($xml->createElement('Value', $package->ReferenceNumber->Value));
-            }
+            //    $refNode->appendChild($xml->createElement('Code', $package->ReferenceNumber->Code));
+            //    $refNode->appendChild($xml->createElement('Value', $package->ReferenceNumber->Value));
+            //}
 
             if (isset($package->AdditionalHandling)) {
                 // $refNode->appendChild($xml->createElement('AdditionalHandling'));
